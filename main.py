09919256aea44d32b1bcfab8e9645e20ac75b8fc
fellipe1908 <128155054+fellipe1908@users.noreply.github.com>
2023-05-17 14:09:37 +0000

    Não está dando certo

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 """Module with functions."""
 
 
-def largest_sum(numbers: list[int]) -> tuple[int, int]:
-    """Encontra e retorna os dois números que somados dão o maior valor."""
-    primeiro = 0  # o primeiro número da soma
-    segundo = 0  # o segundo número da soma
-    return primeiro, segundo
+#def largest_sum(numbers: list[int]) -> tuple[int, int]:
+    #"""Encontra e retorna os dois números que somados dão o maior valor."""
+    #primeiro = 0  # o primeiro número da soma
+    #segundo = 0  # o segundo número da soma
+    #return primeiro, segundo
+def largest_sum(numbers: list[1,2,3,4,5,10]) -> tuple[int, int]:
+    m1 = max(numbers)
+    return m1
+    numbers.remove(m1)
+    m2 = max(numbers)
+    return m2
+    print(m1,m2)
+
+h = [10,9,8,5,30,20,5]
+m1 = max(h)
+print(m1)
+h.remove(m1)
+m2 = max(h)
+print(m2)
+print(m1+m2)
